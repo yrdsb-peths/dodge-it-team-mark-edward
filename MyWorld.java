5908948c075388917f9ea18fbbf67855d838625b
edwardw878 <edwardwang878@gmail.com>
2025-04-22 14:01:54 -0400

    Added hero + rocket

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 500, 100);
+        
+        Rocket rocket = new Rocket();
+        addObject (rocket, 100, 100);
     }
 }
